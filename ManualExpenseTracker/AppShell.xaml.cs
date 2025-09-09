1b718fcf274cae1f7d5c0608dd5fae1c5edceb77
Danie Otto <danieotto@live.com>
2025-09-09 20:12:24 +0200

    Cleanup build artifacts and apply .gitignore

---
+++ ManualExpenseTracker/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ManualExpenseTracker;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ManualExpenseTracker/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ManualExpenseTracker")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ManualExpenseTracker.Pages")]
